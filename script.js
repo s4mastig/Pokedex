511a5b03c989a09f8c52ef481ff22a1f36670d3d
Maximilian Stigler <maximilian.stigler@gmx.net>
2024-07-29 18:31:30 +0200

    responsive and partly refactored

--- script.js
+++ script.js
@@ -6,15 +6,11 @@ const limit = 20;
 async function init() {
     loadingSpinner.style.display = 'flex';
     await getDataPokemon();
-    console.log(pokemonData);
 
-
-    // Simulate a delay for loading
     setTimeout(() => {
         renderPokemon();
-        // Hide the spinner
         loadingSpinner.style.display = 'none';
-    }, 1500); // Adjust the timeout duration as needed
+    }, 1500); 
 }
 
 async function getDataPokemon() {
@@ -30,13 +26,13 @@ function renderPokemon() {
     for (let i = offset; i < offset + limit; i++) {
         let singlePokemon = pokemonData[i];
         let button = document.getElementById('loadButton');
+        
         if (!singlePokemon) {
             button.innerHTML = '';
             break;
         }
 
         let content = document.getElementById('content');
-
         let pokemonName = singlePokemon.name;
         pokemonName = capitalizeFirstLetter(pokemonName);
         let pokemonImg = singlePokemon.sprites.other["official-artwork"].front_default;
@@ -79,44 +75,18 @@ function checkIfType2DivOpen(type2) {
     return type2Div;
 }
 
-
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function renderPokemonHtml(i, id, type1, pokemonName, type2Div, pokemonImg) {
-    return `
-           <div class="poke-card ${type1}" onclick="openEntry(${i})">
-                <div class="poke-id">
-                    <div>#${id}</div>
-                </div>
-                <div class="name-span">
-                    <span>${pokemonName}</span>
-                </div>
-                <div class="lower-section">
-                    <div class="types">
-                        <div class="type ${type1}2">${type1}</div>
-                        ${type2Div}
-                    </div>
-                    <img class="poke-img" src="${pokemonImg}">
-                </div>
-            </div>
-            `;
-}
-
-async function loadMore() {
+function loadMore() {
     let loadingSpinner = document.getElementById('loadingSpinner');
-
-    // Show the spinner
     loadingSpinner.style.display = 'flex';
 
-    // Simulate a delay for loading
     setTimeout(() => {
         renderPokemon();
-        // Hide the spinner
         loadingSpinner.style.display = 'none';
-    }, 1500); // Adjust the timeout duration as needed
+    }, 1500); 
 }
 
 async function filterPokedex() {
@@ -140,24 +110,24 @@ async function filterPokedex() {
         let type2Div = checkIfType2Div(type2);
         let id = singlePokemon.id;
 
-        button.innerHTML = '';
+        button.innerHTML = ''; 
+        if (search.length < 3 && search.length > 0) {
+            content.innerHTML = '';
+            break;
+        }   
         content.innerHTML += renderPokemonHtml(originalIndex, id, type1, pokemonName, type2Div, pokemonImg);
+        
     }
     if (search === '') {
         loadingSpinner.style.display = 'flex';
         offset = 0;
         content.innerHTML = '';
         setTimeout(() => {
-
             renderPokemon();
-            // Hide the spinner
             loadingSpinner.style.display = 'none';
-        }, 1500); // Adjust the timeout duration as needed
-
-
+        }, 1500); 
     }
 }
-
 
 function openEntry(i) {
     content = document.getElementById('content');
@@ -169,47 +139,19 @@ function openEntry(i) {
     let types = singlePokemon.types;
     let type1 = types[0].type.name;
     type1 = capitalizeFirstLetter(type1);
-    let type2 = checkIfType2(types);
-    let type2Div = checkIfType2Div(type2);
-    let id = singlePokemon.id;
 
-    content.innerHTML += `
-    <div id="popUp" class="large-background" onclick="closeCard()">
-        <div class="poke-card-open" onclick="event.stopPropagation()">
-            <div class="${type1} card-1st-half">
-                <img src="${pokemonImg}">
-            </div>
-            <div class="card-2nd-half">
-                <div class="card-buttons">
-                    <div id="buttonLeft" class="button-left" onclick="loadInfo(${i})">Info</div>
-                    <div id="buttonRight" class="button-right button-clicked" onclick="loadStats(${i})">Stats</div>
-                </div>
-                <div id="info" class="info">
-                    
-                </div>
-                <div class="arrows">
-                    <img class="arrow-left" src="./img/arrowleft.png" onclick="nextLeft(${i})">
-                    <img class="arrow-right" src="./img/arrowright.png" onclick="nextRight(${i})">
-                </div>
-                <div id="separator">
-                </div>
-                <div id="separator2">
-                </div>
-            <div>
-        </div>    
-    </div>
-    
-    `;
+    content.innerHTML += openEntryHtml(type1, pokemonImg, i);
     loadInfo(i);
 }
+
 function loadInfo(i) {
     colorButtons();
     let singlePokemon = pokemonData[i];
     let pokemonName = singlePokemon.name
     let pokemonHeight = singlePokemon.height;
     let pokemonWeight = singlePokemon.weight;
-    pokemonWeight = pokemonWeight /10;
-    pokemonHeight = pokemonHeight /10;
+    pokemonWeight = pokemonWeight / 10;
+    pokemonHeight = pokemonHeight / 10;
     pokemonName = capitalizeFirstLetter(pokemonName);
     let types = singlePokemon.types;
     let type1 = types[0].type.name;
@@ -222,43 +164,7 @@ function loadInfo(i) {
     document.getElementById('info').classList.add('info');
 
     infos.innerHTML = '';
-    infos.innerHTML = `
-        <div class="info-left">
-            <div class="single-info">
-                <span>Species:</span>
-                <div class="info-width">${pokemonName}</div>
-            </div>
-            <div class="single-info">
-                <span>Number:</span>
-                <div class="info-width">${id}</div>
-            </div>
-            <div class="single-info">
-                <span>Height:</span>
-                <div class="info-width">${pokemonHeight.toFixed(2).replace('.', ',') + ' '}m</div>
-            </div>
-            <div class="single-info">
-                <span>Weight:</span>
-                <div class="info-width">${pokemonWeight.toFixed(1).replace('.', ',') + ' '}kg</div>
-            </div>
-        </div>
-        <div class="info-right">
-            <div class="single-info">
-                <span>Type:</span>
-            </div>
-             <div class="single-info">
-                <div class="types-flex info-width">
-                    <div class="type-open ${type1}">${type1}</div>
-                    ${type2Div}
-                </div>
-            </div>
-            <div class="single-info">
-                <span>Cry:</span>
-            </div>
-            <div class="single-info">
-                <button onclick="cry(${i})" class="sound-button"><div>Sound</div><img src="./img/sound.png"></button>
-            </div>
-        </div> 
-    `;
+    infos.innerHTML = loadInfoHtml(pokemonName, id, pokemonHeight, pokemonWeight, type1, type2Div, i);
     document.getElementById('separator').classList.toggle('separator');
     document.getElementById('separator2').classList.toggle('separator2');
 }
@@ -342,4 +248,93 @@ function nextLeft(i) {
         i--
         openEntry(i);
     }
+}
+
+function renderPokemonHtml(i, id, type1, pokemonName, type2Div, pokemonImg) {
+    return `
+    <div class="poke-card ${type1}" onclick="openEntry(${i})">
+        <div class="poke-id">
+            <div>#${id}</div>
+        </div>
+        <div class="name-span">
+            <span>${pokemonName}</span>
+        </div>
+        <div class="lower-section">
+            <div class="types">
+                <div class="type ${type1}2">${type1}</div>
+                ${type2Div}
+            </div>
+            <img class="poke-img" src="${pokemonImg}">
+        </div>
+    </div>
+    `;       
+}
+
+function openEntryHtml(type1, pokemonImg, i) {
+    return `
+    <div id="popUp" class="large-background" onclick="closeCard()">
+        <div class="poke-card-open" onclick="event.stopPropagation()">
+            <div class="${type1} card-1st-half">
+                <button class="close-card-button" onclick="closeCard()"><b>X</b></button>
+                <img src="${pokemonImg}">
+            </div>
+            <div class="card-2nd-half">
+                <div class="card-buttons">
+                    <div id="buttonLeft" class="button-left" onclick="loadInfo(${i})">Info</div>
+                    <div id="buttonRight" class="button-right button-clicked" onclick="loadStats(${i})">Stats</div>
+                </div>
+                <div id="info" class="info"> 
+                </div>
+                <div class="arrows">
+                    <img class="arrow-left" src="./img/arrowleft.png" onclick="nextLeft(${i})">
+                    <img class="arrow-right" src="./img/arrowright.png" onclick="nextRight(${i})">
+                </div>
+                <div id="separator">
+                </div>
+                <div id="separator2">
+                </div>
+            <div>
+        </div>    
+    </div>
+    `;
+}
+
+function loadInfoHtml(pokemonName, id, pokemonHeight, pokemonWeight, type1, type2Div, i) {
+    return `
+    <div class="info-left">
+        <div class="single-info">
+            <span>Species:</span>
+            <div class="info-width">${pokemonName}</div>
+        </div>
+        <div class="single-info">
+            <span>Number:</span>
+            <div class="info-width">${id}</div>
+        </div>
+        <div class="single-info">
+            <span>Height:</span>
+            <div class="info-width">${pokemonHeight.toFixed(2).replace('.', ',') + ' '}m</div>
+        </div>
+        <div class="single-info">
+            <span>Weight:</span>
+            <div class="info-width">${pokemonWeight.toFixed(1).replace('.', ',') + ' '}kg</div>
+        </div>
+    </div>
+    <div class="info-right">
+        <div class="single-info-right">
+            <span>Type:</span>
+        </div>
+        <div class="single-info-right">
+            <div class="types-flex">
+                <div class="type-open ${type1}">${type1}</div>
+                ${type2Div}
+            </div>
+        </div>
+        <div class="single-info-right">
+            <span>Cry:</span>
+        </div>
+        <div class="single-info-right">
+            <button onclick="cry(${i})" class="sound-button"><div>Sound</div><img src="./img/sound.png"></button>
+        </div>
+    </div> 
+`;
 }
